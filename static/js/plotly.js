b32d5c188af24d2bd262e020e6c070874da62d2a
Robert Gonzalez <robertsgonzalez92@yahoo.com>
2022-07-10 22:26:50 -0500

    html clean up

--- static/js/plotly.js
+++ static/js/plotly.js
@@ -1,2 +1,19 @@
 
+d3.json(scrapedMakeUrl).then((data) => {
+  // console.log(data.names)
+
+  let dropdown = d3.select("#selscrapemake");
+
+  data.make.forEach((id) => {
+      console.log(id);
+
+      dropdown.append("option").text(id).property("value", id);
+    // for (let i=0; i < 5; i++){
+    //   dropdown.append("option").text("blank").property("value", "blank");
+    // }
+
+    
+  });
+})
+
 
