f50a1f9c9c709e1c3e8614ef0d32529f8624cdb2
Robert Lane <RobertLaneJr@gmail.com>
2022-07-10 22:50:38 -0500

    step one of plotly pulldown

--- static/js/plotly.js
+++ static/js/plotly.js
@@ -1,2 +1,19 @@
+var kaggleMakeUrl = "api/v1.0/kaggle/makes"
 
+d3.json(kaggleMakeUrl).then((data) => {
+  // console.log(data.names)
 
+  let dropdown = d3.select("#sel_kaggle_make");
+
+  data.make.forEach((id) => {
+      console.log(id);
+
+      dropdown.append("option").text(id).property("value", id);
+    // for (let i=0; i < 5; i++){
+    //   dropdown.append("option").text("blank").property("value", "blank");
+    // }
+
+    
+  });
+  // build charts here
+})
